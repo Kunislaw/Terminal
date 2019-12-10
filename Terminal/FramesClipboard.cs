51decbb33ea50436c3300ec0f5281b03b6242e24
Przemyslaw Sulecki <przemek.sulecki.mail@gmail.com>
2019-12-10 20:02:02 +0100

    Add model for frame in clipboard

---
+++ Terminal/FramesClipboard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terminal
+{
+    public class FramesClipboard
+    {
+        public string creationTimestamp;
+        public string lastModification = null;
+        public string name { get; set; }
+        public byte[] frame { get; set; }
+
+        public FramesClipboard()
+        {
+            creationTimestamp = DateTime.Now.ToString("d-M-y HH:mm:ss", System.Globalization.DateTimeFormatInfo.InvariantInfo);
+        }
+    }
+}
